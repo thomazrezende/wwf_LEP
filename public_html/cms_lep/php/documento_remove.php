71a1f6aaf50ac2d3fd6302acaeec100a00de84c2
thomazrezende <thomaz.rezende@gmail.com>
2015-03-04 12:48:54 -0300

    REMOVE DOCUMENTO

--- public_html/cms_lep/php/documento_remove.php
+++ public_html/cms_lep/php/documento_remove.php
@@ -1,18 +1,20 @@
 <?php  
 
+	
 	require_once("../../../_tr_1048672/seguranca.php"); 
 	require_once("../../../_tr_1048672/acesso.php");
 	require_once("../_tr/html.php"); 
+	require_once("../_tr/mysql.php"); 
 	require_once("../_tr/arquivo.php");
 	
 	conectar();
 	verif_log();
 	
-	$dados = sql_select("documentos","arquivo","","id=".$id,false);   
-			
+	$id = $_GET['id'];
+
+	remove_pasta('../../documentos/documento'.$id, 'documentos', $id, false, false);
+	//remove_pasta($path, $tabela, $id, $tabelas_vinc, $lbs_vinc){ 
 	
-	$ok = unlink("../documentos/".); 
-	
-	location("../thomaz_curriculo.php?msg_ok=ARQUIVO REMOVIDO COM SUCESSO","");
+	location("../documentos.php","msg_ok=DOCUMENTO REMOVIDO COM SUCESSO");
 	
 ?>
